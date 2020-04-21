6e35a63e7c21a86b3b745938c4284bcc47e873cb
Tom Cools <tom.cools@live.be>
2020-04-21 22:10:43 +0200

    Removed unused dependency.

--- core/src/test/java/be/tomcools/javamod/core/impl/BeerBarImplTest.java
+++ core/src/test/java/be/tomcools/javamod/core/impl/BeerBarImplTest.java
@@ -1,15 +1,23 @@
 package be.tomcools.javamod.core.impl;
 
 import be.tomcools.javamod.core.dependencies.BeerRepository;
+import be.tomcools.javamod.core.domain.Beer;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class BeerBarImplTest {
-    @Test
-    public void init() {
-        BeerBarImpl bar = new BeerBarImpl(Mockito.mock(BeerRepository.class));
 
-        //throw new RuntimeException("FAIL");
-        //bar.getBeerMenu();
+    BeerRepository repo = mock(BeerRepository.class);
+
+    BeerBarImpl sut = new BeerBarImpl(repo);
+
+    @Test
+    public void simpleShowcaseTest() {
+        sut.addBeerToMenu(new Beer("TestBeer", 2.3));
+
+        verify(repo).addBeerToMenu(any());
     }
 }
